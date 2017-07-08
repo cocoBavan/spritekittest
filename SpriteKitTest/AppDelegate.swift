c57e0c901bc431c7508c56b2288cb63e954f61b4
Bavan Palan <bavan@fetchtv.com.au>
2017-07-08 17:20:51 +1000

    Initial Commit

---
+++ SpriteKitTest/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SpriteKitTest
+//
+//  Created by Bavan Palan on 8/7/17.
+//  Copyright © 2017 Bavan Palan. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SpriteKitTest/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SpriteKitTest
+//
+//  Created by Bavan Palan on 8/7/17.
+//  Copyright © 2017 Bavan Palan. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
